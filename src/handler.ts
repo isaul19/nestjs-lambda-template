4af00aa1b820488ef4199e9534a5520a6e8d1d46
isaul19 <saulhola348@gmail.com>
2024-04-05 08:34:13 -0500

    update handle from main

--- src/handler.ts
+++ src/handler.ts
@@ -1,4 +1,5 @@
 import { configure as serverlessExpress } from '@vendia/serverless-express';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
@@ -9,6 +10,15 @@ export const handler = async (event, context: Context, callback: any) => {
   if (!cachedServer) {
     const nestApp = await NestFactory.create(AppModule);
     await nestApp.init();
+
+    nestApp.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // Deja entrar solo campos permitidos
+        forbidNonWhitelisted: true, // Tira error en caso de que haya un campo no permitido
+        transform: true, // Trata de transformar string a number, etc.
+      }),
+    );
+
     cachedServer = serverlessExpress({
       app: nestApp.getHttpAdapter().getInstance(),
     });
